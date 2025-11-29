c58c26e9abb252bb03fc67648982532885de015f
Diventa Computers <diventacomputers@gmail.com>
2025-11-29 17:14:06 -0500

    monitor banner added

--- src/components/ui/HeroBanner.jsx
+++ src/components/ui/HeroBanner.jsx
@@ -7,12 +7,14 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules'
 import banner1 from '../../assets/banners/banner1.svg'
 import banner2 from '../../assets/banners/banner2.svg'
 import banner3 from '../../assets/banners/banner3.svg'
+import monitor from '../../assets/images/monitor.png'
 
 function HeroBanner() {
   const banners = [
     { id: 1, image: banner1, title: 'Alcanza tu máximo rendimiento', subtitle: 'Nueva serie Alienware', text: 'Pre-Venta Exclusiva' },
     { id: 2, image: banner2, title: 'Gaming sin límites', subtitle: 'RTX 5000 Series', text: 'Solo esta semana' },
     { id: 3, image: banner3, title: 'Ofertas en Accesorios', subtitle: 'Audio y periféricos', text: 'Hasta 50% OFF' },
+    { id: 4, image: monitor, title: 'Monitores Corporativos', subtitle: '4k / Curvos / Thunderbold / IPS BLACK', text: 'Ver producto' },
   ]
 
   return (
